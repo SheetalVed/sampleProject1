d4ed9a548edd86ef56fda88b798f9c9a592a8b5b
Sheetal <sheetalved2991@gmail.com>
2024-07-19 08:10:08 +0200

    added docker config

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Make sure Vite listens on all network interfaces
+    port: 5173, // Port should match the port you expose in Docker
+  },
   plugins: [
     vue(),
   ],
